4542097f9dccd6d221be7a93dd6a30d337c0f721
John Crepezzi <john.crepezzi@gmail.com>
2011-03-08 16:12:50 -0500

    Allow for counts to be passed with the needed # of gotchas

--- lib/gotcha/controller_helpers.rb
+++ lib/gotcha/controller_helpers.rb
@@ -7,13 +7,13 @@ module Gotcha
     end 
 
     # return a true / false as to whether or not *all* of the gotchas on the page validated
-    def gotcha_valid?
-      @_gotcha_validated ||= determine_gotcha_validity
+    def gotcha_valid?(expected = 1)
+      @_gotcha_validated ||= determine_gotcha_validity(expected)
     end
       
     # Validate the gotcha, throw an exception if the gotcha does not validate (any on the page)
-    def validate_gotcha!
-      raise Gotcha::ValidationError.new unless validate_gotcha
+    def validate_gotcha!(expected = 1)
+      raise Gotcha::ValidationError.new unless gotcha_valid?(expected)
     end
 
     private
@@ -21,8 +21,9 @@ module Gotcha
     # Go through each response, using the down_transform
     # of the original class (as long as it is a subclass of Gotcha::Base)
     # and compare the hash to the hash of the value
-    def determine_gotcha_validity
+    def determine_gotcha_validity(expected_gotcha_count = 1)
       return false unless params[:gotcha_response].kind_of?(Enumerable)    
+      return false unless params[:gotcha_response].count == expected_gotcha_count
       params[:gotcha_response].all? do |ident, value|
         type, hash = ident.split '-'
         return false unless Object.const_defined?(type)
